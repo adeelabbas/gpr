ab76e60810060517d1637f869e541b36cbef0d39
Adeel Abbas <abbas.adeel@gmail.com>
2026-09-19 21:20:25 -0700

    gpr_tools: long option names
    Options get descriptive snake_case names (--input_path, --output_path,
    --input_pixel_format, --print_metadata, --apply_metadata, --gpmf_path,
    --rgb_resolution, --output_ppm_bits) and keep their short forms. The
    width, height, pitch and pixel format default to the input's metadata
    instead of 4000x3000 rggb14, and the help text says when each option
    applies.

--- source/app/gpr_tools/main.cpp
+++ source/app/gpr_tools/main.cpp
@@ -32,40 +32,30 @@ using namespace std;
 
 class my_argument_parser : public argument_parser
 {
-protected:
-    
     bool    help;
-    
     bool    verbose;
 
 public:
-    
-    bool    dump_gpr_parameters;
-    
     string  jpg_preview_file_path;
 
+    bool    print_metadata;
+    string  apply_metadata;
+
+    string  input_path;
     int     input_width;
-    
     int     input_height;
-
     int     input_pitch;
-    
-    int     input_skip_rows;
-    
     string  input_pixel_format;
-    
-    string  input_file_path;
-    
-    string  gpmf_file_path;
+    int     input_skip_rows;
 
-    string  rgb_file_resolution;
-    
-    int     rgb_file_bits;
-    
-    string  output_file_path;
+    string  output_path;
 
-    string  apply_gpr_parameters;
+    string  gpmf_path;
+
+    string  rgb_resolution;
     
+    int     output_ppm_bits;
+
 public:
 
     bool get_verbose() { return verbose; }
@@ -75,35 +65,33 @@ public:
     void set_options()
     {
         command_options.addOptions()
-        /* long and short name */                           /* variable to update */                    /* default value */     /* help text */
-        ("help",                                            help,                                       false,                  "Prints this help text")
+        /* long and short name */   /* variables */       /* default value */     /* help text */
+        ("help",                    help,                 false,            "Prints this help text")
         
-        ("verbose",                                         verbose,                                    false,                  "Verbosity of the output")
+        ("verbose,v",               verbose,              false,            "Verbosity of the output")
 
-        ("JpgPreviewFilePath,P",                            jpg_preview_file_path,                      string(""),             "Preview jpg file path")
+        ("preview_file_path",       jpg_preview_file_path, string(""),      "Preview jpg file path")
         
-        ("DumpGprParameters,d",                             dump_gpr_parameters,                        false,                  "Dump GPR parameters to standard output")
+        ("print_metadata,d",        print_metadata,       false,            "Print gpr params (as json) to standard output")
+        ("apply_metadata,a",        apply_metadata,       string(""),       "Use gpr params for GPR/DNG metadata")
 
-        ("InputSkipRows,s",                                 input_skip_rows,                            0,                      "Input image rows to skip")
+        ("input_path,i",            input_path,           string(""),       "Input file path \n"
+                                                                            "File Choices: GPR, DNG, RAW")
+        
+        ("input_width,w",           input_width,          0,                "Input image width in pixel samples")
+        ("input_height,h",          input_height,         0,                "Input image height in pixel samples")
+        ("input_pitch,p",           input_pitch,          0,                "Input image pitch in bytes")
+        ("input_pixel_format,x",    input_pixel_format,   string(""),       "Input pixel format \n"
+                                                                            "Choices: rggb12, rggb12p, [rggb14], gbrg12, gbrg12p, bggr12, bggr14 \n")
+        ("input_skip_rows",         input_skip_rows,      0,                "Input image rows to skip (shifts Bayer phase, e.g. BGGR->GRBG)")
 
-        ("InputFilePath,i",                                 input_file_path,                            string(""),             "Input file path.\n(files types: GPR, DNG, RAW)")
-        
-        ("InputWidth,w",                                    input_width,                                4000,                   "Input image width in pixel samples [4000]")
-        
-        ("InputHeight,h",                                   input_height,                               3000,                   "Input image height in pixel samples [3000]")
+        ("output_path,o",           output_path,          string(""),       "Output file path.\n"
+                                                                            "File choices: GPR, DNG, PPM, RAW, JPG")
+        ("gpmf_path,g",             gpmf_path,            string(""),       "GPMF file path")
 
-        ("InputPitch,p",                                    input_pitch,                                8000,                   "Input image pitch in bytes [8000]")
-        
-        ("InputPixelFormat,x",                              input_pixel_format,                         string("rggb14"),       "Input pixel format \n(rggb12, rggb12p, [rggb14], gbrg12, gbrg12p)")
-        
-        ("ApplyGprParameters,a",                            apply_gpr_parameters,                       string(""),             "Parameters to use for GPR or DNG file.")
-        
-        ("GPMFFilePath,g",                                  gpmf_file_path,                             string(""),             "GPMF file path")
-
-        ("RgbFileResolution,r",                             rgb_file_resolution,                        string(""),             "Output RGB resolution \n[1:1, 2:1, 4:1, 8:1. 16:1]")
-        ("RgbFileBits,b",                                   rgb_file_bits,                              8,                      "Output RGB bits [8]")
-
-        ("OutputFilePath,o",                                output_file_path,                           string(""),             "Output file path.\n(files types: GPR, DNG, PPM, RAW, JPG)");
+        ("rgb_resolution",          rgb_resolution,       string(""),       "Output RGB resolution. Only applicable when output format is PPM or JPG \n"
+                                                                            "Choices: 1:1, 2:1, [4:1], 8:1. 16:1")
+        ("output_ppm_bits",         output_ppm_bits,      8,                "Output bits, use only with PPM output. Choices [8], 16")
         ;
     }
 };
@@ -147,46 +135,51 @@ int main(int argc, char *argv [])
     if( args.parse(argc, argv, line, zerotag) )
     {
         printf("\n");
+        printf("Following parameters override metadata when set and are only used when the input format is RAW: \n");
+        printf("    input_width, input_height, input_pitch, input_pixel_format, input_skip_rows and input_skip_cols \n");
+        printf("\n");
+        printf("\n");
         printf("-- Example Commnads (please see data/tests/run_tests.sh for more examples) --\n");
         printf("GPR to DNG: \n");
-        printf("  %s -i ./data/samples/Hero6/GOPR0024.GPR -o ./data/samples/Hero6/GOPR0024.DNG \n\n", argv[0] );
+        printf("  %s -i ./data/samples/HERO6.GPR -o ./data/samples/HERO6.DNG \n\n", argv[0] );
         printf("GPR to RGB (PPM format in 1000x750 resolution): \n");
-        printf("  %s -i ./data/samples/Hero6/GOPR0024.GPR -o ./data/samples/Hero6/GOPR0024.PPM -r 4:1 \n\n", argv[0] );
+        printf("  %s -i ./data/samples/HERO6.GPR -o ./data/samples/HERO6.PPM -r 4:1 \n\n", argv[0] );
         printf("GPR to RGB (JPG format in 500x375 resolution): \n");
-        printf("  %s -i ./data/samples/Hero6/GOPR0024.GPR -o ./data/samples/Hero6/GOPR0024.JPG -r 8:1 \n\n", argv[0] );
+        printf("  %s -i ./data/samples/HERO6.GPR -o ./data/samples/HERO6.JPG -r 8:1 \n\n", argv[0] );
         printf("Analyze a GPR or DNG file and output metadata parameters to a file: \n");
-        printf("  %s -i ./data/samples/Hero6/GOPR0024.GPR -d 1 > ./data/samples/Hero6/GOPR0024.TXT \n\n", argv[0] );
+        printf("  %s -i ./data/samples/HERO6.GPR -d 1 > ./data/samples/HERO6.TXT \n\n", argv[0] );
         printf("Read RAW pixel data, along with gpr parameters (from a file) and apply to an output GPR or DNG file: \n");
-        printf("  %s -i ./data/samples/Hero6/GOPR0024.RAW -o ./data/samples/Hero6/GOPR0024.DNG -a ./data/samples/Hero6/GOPR0024.TXT \n\n", argv[0] );
+        printf("  %s -i ./data/samples/HERO6.RAW -o ./data/samples/HERO6.DNG -a ./data/samples/HERO6.TXT \n\n", argv[0] );
 
         return -1;
     }
     
-    if( args.dump_gpr_parameters )
+    if( args.print_metadata )
     {
-        if( dng_dump(args.input_file_path.c_str()) != 0 )
+        if( dng_dump(args.input_path.c_str()) != 0 )
             return -1;
     }
     else
     {
-        string ext = strrchr( args.input_file_path.c_str(),'.');
+        string ext = strrchr( args.input_path.c_str(),'.');
         
-        if( args.output_file_path == string("") && ( ext == string(".GPR") || ext == string(".gpr") ) )
+        if( args.output_path == string("") && ( ext == string(".GPR") || ext == string(".gpr") ) )
         {
-            args.output_file_path = args.input_file_path;
-            args.output_file_path.erase(args.output_file_path.find_last_of("."), string::npos);
+            args.output_path = args.input_path;
+            args.output_path.erase(args.output_path.find_last_of("."), string::npos);
             
-            args.output_file_path = args.output_file_path + string(".DNG");
+            args.output_path = args.output_path + string(".DNG");
         }
     }
     
-    fprintf( stderr, "%s Input File: %s \n",    zerotag, args.input_file_path.c_str() );
-    fprintf( stderr, "%s Output File: %s \n",   zerotag, args.output_file_path.c_str() );
+    // We are not calling LogPrint here because that is an internal functionality to gpr_sdk
+    fprintf( stderr, "%s Input File: %s \n",    zerotag, args.input_path.c_str() );
+    fprintf( stderr, "%s Output File: %s \n",   zerotag, args.output_path.c_str() );
 
-    if( args.output_file_path != "" )
+    if( args.output_path != "" )
     {
-        return dng_convert_main(args.input_file_path.c_str(), args.input_width, args.input_height, args.input_pitch, args.input_skip_rows, args.input_pixel_format.c_str(),
-                                args.output_file_path.c_str(), args.apply_gpr_parameters.c_str(), args.gpmf_file_path.c_str(), args.rgb_file_resolution.c_str(), args.rgb_file_bits,
+        return dng_convert_main(args.input_path.c_str(), args.input_width, args.input_height, args.input_pitch, args.input_skip_rows, args.input_pixel_format.c_str(),
+                                args.output_path.c_str(), args.apply_metadata.c_str(), args.gpmf_path.c_str(), args.rgb_resolution.c_str(), args.output_ppm_bits,
                                 args.jpg_preview_file_path.c_str() );
     }
     
